b9e601d3d114a9ba50617868df69d9eff5968435
Vent Lam <ventlcc@gmail.com>
2012-11-16 21:36:29 +0800

    Signed-off-by: Vent Lam <ventlcc@gmail.com>

---
+++ src/main/java/edu/sirc/dm/App.java
@@ -0,0 +1,13 @@
+package edu.sirc.dm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
