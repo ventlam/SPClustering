10a00af46833c48c4d3d45c42e38f6f67dde86ef
Vent Lam <ventlcc@gmail.com>
2012-11-16 21:44:50 +0800

    KMeans

---
+++ src/main/java/edu/sirc/dm/kmeans_result.java
@@ -0,0 +1,12 @@
+package edu.sirc.dm;
+
+/**
+ * 
+ * @author Yuanbo She
+ * 
+ */
+public class kmeans_result {
+    public int attempts; // 退出迭代时的尝试次数
+    public double criteriaBreakCondition; // 退出迭代时的最大距离（小于阈值）
+    public int k; // 聚类数
+}
